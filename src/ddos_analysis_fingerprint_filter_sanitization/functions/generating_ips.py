6491802ba93a3ba1d9aa645e8247386f29dfdb43
erev0s <erev0s@mail.com>
2018-03-20 11:28:06 +0100

    minor update

--- src/ddos_analysis_fingerprint_filter_sanitization/functions/generating_ips.py
+++ src/ddos_analysis_fingerprint_filter_sanitization/functions/generating_ips.py
@@ -6,9 +6,10 @@
 
 import random
 
-ips =[]
+
 
 def generating_ips(int):
+    ips =[]
     for x in range(0,int):
         ip = '{}.{}.{}.{}'.format(*__import__('random').sample(range(0, 255), 4))
         ips.append(ip) 
