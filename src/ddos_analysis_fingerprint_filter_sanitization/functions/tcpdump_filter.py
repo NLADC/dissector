dc295bce63c17971c17e9d2981b5d2a034dfce8b
erev0s <erev0s@mail.com>
2018-03-28 12:11:41 +0200

    updated the filter function for tcpdump

--- src/ddos_analysis_fingerprint_filter_sanitization/functions/tcpdump_filter.py
+++ src/ddos_analysis_fingerprint_filter_sanitization/functions/tcpdump_filter.py
@@ -29,7 +29,7 @@ def tcpdump_filter(input_file, pattern):
 
     # Filter by source IP
     if len(pattern['src_ips']) > 0:
-        src_ip_filter = ["src ip " + ip for ip in pattern['src_ips']]
+        src_ip_filter = ["src host " + ip for ip in pattern['src_ips']]
         output_filter.append(" or ".join(src_ip_filter))
 
     # Filter by port number
@@ -37,7 +37,7 @@ def tcpdump_filter(input_file, pattern):
         src_port_filter = ["src port " + str(port) for port in pattern['src_ports']]
         output_filter.append(" or ".join(src_port_filter))
         
-    filter_out = "(" + ") and (".join(output_filter) + ")"
+    filter_out = "'(" + ")' and '(".join(output_filter) + ")'"
      
     while(1):
         nonce = str(random.randrange(0, 1000, 1))
@@ -45,16 +45,13 @@ def tcpdump_filter(input_file, pattern):
         if os.path.isfile(output_vector) == False:
             break
     
-    filter_path = "/tmp/filter" + nonce
-    f = open(filter_path, "w")
-    f.write(filter_out)
-    f.close()
-    
-    p = subprocess.Popen(["cat " + filter_path + " | tcpdump -r " + input_file + " -w output/" + output_vector + ".pcap"], shell=True, stdout=subprocess.PIPE)
+#     filter_path = "/tmp/filter" + nonce
+#     f = open(filter_path, "w")
+#     f.write(filter_out)
+#     f.close()
+    p = subprocess.Popen(["tcpdump -r " + input_file + " -w output/" + output_vector + ".pcap " + filter_out], shell=True, stdout=subprocess.PIPE)
     p.communicate()
     p.wait()
-    
-    print ' Done!'
     
     return nonce
 
