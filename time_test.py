ee046f36aa5d2f25a686f8cadd8d4cf904c617ca
erev0s <erev0s@mail.com>
2018-04-04 14:21:01 +0200

    updates in tcpdump filter

---
+++ time_test.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[13]:
+
+
+from functions.input2dataframe import *
+from functions.analyse_df_pcap import *
+from functions.analyse_df_nfdump import *
+from functions.nfdump_filter import *
+from functions.tcpdump_filter import *
+from functions.replace_dst import *
+
+
+
+# In[3]:
+
+
+input_file = "input/1.pcap"
+
+
+# In[10]:
+
+
+df = input2dataframe(input_file)
+all_patterns = analyse_df_pcap(df)
+        
+vector_ids = []
+for pattern in all_patterns["patterns"]:
+    vector_ids.append(tcpdump_filter(input_file, pattern))
+
+
+# In[15]:
+
+
+input_pcap = "output/attack_vector_" + vector_ids[0] + ".pcap"
+replace_ipinpcap(input_pcap,all_patterns["dst_ip"])
+
