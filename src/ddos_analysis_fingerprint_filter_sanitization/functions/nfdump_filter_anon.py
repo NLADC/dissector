00ee374e21124290b2618f15ac1529ae73172f0e
Koen van Hove <koen@koenvh.nl>
2018-04-10 16:20:12 +0200

    Added anonimisation step to nfdump filtering

--- src/ddos_analysis_fingerprint_filter_sanitization/functions/nfdump_filter_anon.py
+++ src/ddos_analysis_fingerprint_filter_sanitization/functions/nfdump_filter_anon.py
@@ -18,15 +18,24 @@ def nfdump_filter_anon(input_file, pattern, dst_ip):
     # Filtering based on host/proto and ports
     if len(pattern['src_ports']) > 1:
         filter_out = "dst ip " + dst_ip + " and proto " + str(pattern['ip_protocol']) + " and dst port " + str(pattern["dst_ports"].keys()[0])
-    elif len(pattern['dst_ports']) > 1:
+    else:
         filter_out = "dst ip " + dst_ip + " and proto " + str(pattern['ip_protocol']) + " and src port " + str(pattern["src_ports"].keys()[0])
         
     #proper filename based on start timestamp and selected port
-    filename =  pattern["start_timestamp"].split()[0].replace('-', '') + "_" + str(pattern["selected_port"]) + ".nfdump"
-    print filename
+    timestamp = pattern["start_timestamp"].split()
+    filename =  timestamp[0].replace("-", "") + timestamp[1].replace(":", "") + "_" + str(pattern["selected_port"]) + ".nfdump"
+    
     
     #running nfdump with the filters created above
     p = subprocess.Popen(["nfdump -r " + input_file + " -w output/nfdumptemp " + "'" + filter_out + "'"], shell=True, stdout=subprocess.PIPE)
+    p.communicate()
+    p.wait()
+    
+    p = subprocess.Popen(["functions/nfdump/bin/nfanon -r output/nfdumptemp -w output/" + filename], shell=True, stdout=subprocess.PIPE)
+    p.communicate()
+    p.wait()
+    
+    p = subprocess.Popen(["rm output/nfdumptemp"], shell=True, stdout=subprocess.PIPE)
     p.communicate()
     p.wait()
     #
@@ -36,5 +45,5 @@ def nfdump_filter_anon(input_file, pattern, dst_ip):
     #
     #
     #
-    return "created nfdump temp file - NOT anonymized"
+    
 
