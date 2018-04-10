3774242d75690fc39dcd642d65664e79a7544d8f
jjsantanna <jairsantanna@gmail.com>
2018-04-10 11:23:48 +0200

    gost file

--- src/ddos_analysis_fingerprint_filter_sanitization/functions/nfdump_filter_anon.py
+++ src/ddos_analysis_fingerprint_filter_sanitization/functions/nfdump_filter_anon.py
@@ -1,32 +1,30 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import subprocess
 import os
+import time
 
 
-# In[ ]:
+# In[2]:
 
 
 def nfdump_filter_anon(input_file, pattern, dst_ip):
     
     
     # Filtering based on host/proto and ports
+    print type(pattern['src_ports'])
     if len(pattern['src_ports']) > 1:
         filter_out = "dst ip " + dst_ip + " and proto " + str(pattern['ip_protocol']) + " and dst port " + str(pattern["dst_ports"].keys()[0])
     elif len(pattern['dst_ports']) > 1:
         filter_out = "dst ip " + dst_ip + " and proto " + str(pattern['ip_protocol']) + " and src port " + str(pattern["src_ports"].keys()[0])
         
-    #proper filename based on start timestamp(converted to epoch) and selected port
-    date = str(pattern["start_timestamp"])[:10]
-    formatis = '%Y-%m-%d'
-    epoch = int(time.mktime(time.strptime(date, formatis)))
-
-    filename = str(epoch) + "_" + pattern["selected_port"] + ".nfdump"
-    
+    #proper filename based on start timestamp and selected port
+    filename =  pattern["start_timestamp"].split()[0].replace('-', '') + "_" + pattern["selected_port"] + ".nfdump"
+   
     #running nfdump with the filters created above
     p = subprocess.Popen(["nfdump -r " + input_file + " -w output/nfdumptemp " + "'" + filter_out + "'"], shell=True, stdout=subprocess.PIPE)
     p.communicate()
