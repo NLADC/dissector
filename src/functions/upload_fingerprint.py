7d58a1cddee5e4368b5edd1c17314ddf305fd3a9
Koen van Hove <koen@koenvh.nl>
2018-05-26 17:47:39 +0200

    Add upload file function

---
+++ src/functions/upload_fingerprint.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+
+def upload(pcap_file, fingerprint, username, password, key):
+    files = {
+        "json": ("output.json", json.dumps(fingerprint)),
+        "pcap": open(pcap_file, "rb")
+    }
+    headers = {
+        "X-Username": username,
+        "X-Password": password,
+        "X-Filename": key
+    }
+    r = requests.post("http://localhost:8000/upload-file", files=files, headers=headers)
+
+    print(r.status_code)
