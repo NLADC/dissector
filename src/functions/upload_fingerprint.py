95179c05b2da19a209a87eb1505ce8a07f5a7279
Koen van Hove <koen@koenvh.nl>
2018-05-29 13:10:08 +0200

    Make upload use JSON path

--- src/functions/upload_fingerprint.py
+++ src/functions/upload_fingerprint.py
@@ -2,13 +2,18 @@ import json
 import requests
 
 
-def upload(pcap_file, fingerprint, username, password, key):
-    fingerprint_data = json.dumps(fingerprint)
-    if "key" not in fingerprint_data:
-        fingerprint_data["key"] = key
-
+def upload(pcap_file, fingerprint_path, username, password, key):
+    """
+    Upload a fingerprint and attack vector to DDoSDB
+    :param pcap_file: Path to the pcap file
+    :param fingerprint_path: Path to the fingerprint file
+    :param username: DDoSDB username
+    :param password: DDoSDB password
+    :param key: ID to identify this attack, also the filename of the pcap_file.
+    :return:
+    """
     files = {
-        "json": ("output.json", fingerprint_data),
+        "json": open(fingerprint_path, "rb"),
         "pcap": open(pcap_file, "rb")
     }
     headers = {
