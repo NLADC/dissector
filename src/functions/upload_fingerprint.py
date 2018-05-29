7543a67373a74653d425602962e8a9aa8cfac4f0
Koen van Hove <koen@koenvh.nl>
2018-05-29 12:38:31 +0200

    Add key if it does not exist in the JSON

--- src/functions/upload_fingerprint.py
+++ src/functions/upload_fingerprint.py
@@ -3,8 +3,12 @@ import requests
 
 
 def upload(pcap_file, fingerprint, username, password, key):
+    fingerprint_data = json.dumps(fingerprint)
+    if "key" not in fingerprint_data:
+        fingerprint_data["key"] = key
+
     files = {
-        "json": ("output.json", json.dumps(fingerprint)),
+        "json": ("output.json", fingerprint_data),
         "pcap": open(pcap_file, "rb")
     }
     headers = {
