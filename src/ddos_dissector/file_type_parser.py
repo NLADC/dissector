883a0489c98c9fc72937da3d4e29736917b9a58a
jjsantanna <jairsantanna@gmail.com>
2018-09-28 14:23:26 +0200

    decoding the tcp.flags.str

--- src/ddos_dissector/file_type_parser.py
+++ src/ddos_dissector/file_type_parser.py
@@ -124,9 +124,9 @@ def convert_pcap_to_dataframe(input_file):
     if 'tcp.flags.str' in df.columns:
         df['tcp.flags.str'] = df['tcp.flags.str'].str.encode("utf-8")  
 
-    df['ip.ttl'] = df['ip.ttl'].apply(lambda x: int(x) if str(x).isdigit() else None)
+    df['ip.ttl'] = df['ip.ttl']
+    df['tcp.flags.str'] = df['tcp.flags.str'].str.decode("utf-8")
 
-    print(df['tcp.flags.str'])
     return df
 
 
