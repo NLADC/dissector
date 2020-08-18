db46456e20fa371fde0a07f55b48bd056c9c32db
Joao Ceron <ceron@botlog.org>
2020-08-17 21:03:18 -0300

    new release house keeping

--- src/ddos_dissector/__init__.py
+++
@@ -1,6 +0,0 @@
-from ddos_dissector.file_type_parser import *
-from ddos_dissector.dataframe_analysis import *
-from ddos_dissector.attack_vector_anonymizer import *
-from ddos_dissector.upload_fingerprint import *
-
-name = "ddos_dissector"
--- src/ddos_dissector/exceptions/UnsupportedFileTypeError.py
+++
@@ -1,5 +0,0 @@
-class UnsupportedFileTypeError(Exception):
-    """
-    Error when the file used is not supported by the system
-    """
-    pass
